68638c54e37fd8cf50fbae7e2161c2987e0a21f9
Vizziona <vizzionainc@gmail.com>
2025-01-19 16:15:21 +0200

    Budget page to handle budget functionality

---
+++ frontend/src/pages/Budget.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BudgetForm from '../components/BudgetForm';
+
+const Budget = () => {
+  return (
+    <div>
+      <h1>Budget</h1>
+      <BudgetForm />
+    </div>
+  );
+};
+
+export default Budget;
